a55c4387a08b7f87972bdc2427cc2be4c9284af7
Blaine <Chenghao.Fan@outlook.com>
2022-05-30 15:07:36 +0800

    version 1.0

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Chenghao Fan
+ * @date 2022/5/30 14:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello, git!");
+    }
+}
